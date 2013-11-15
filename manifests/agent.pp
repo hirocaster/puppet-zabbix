1c8bd55346fd59e96acee4ce0b9ccfbda69c4baa
hirocaster <hohtsuka@gmail.com>
2013-11-15 23:05:40 +0900

    Fix need repository

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -2,20 +2,21 @@
 define zabbix::agent(
   $zabbix_server = '127.0.0.1'
   ) {
-  package { 'zabbix-agent':
-    require => Apt::Source['zabbix']
-  }
+    include zabbix::repo
+    package { 'zabbix-agent':
+      require => Apt::Source['zabbix']
+    }
 
-  file { 'zabbix-agent-conf':
-    path    => '/etc/zabbix/zabbix_agentd.conf',
-    content => template('zabbix/zabbix_agentd.conf.erb'),
-    require => Package['zabbix-agent'],
-    notify  => Service['zabbix-agent']
-  }
+    file { 'zabbix-agent-conf':
+      path    => '/etc/zabbix/zabbix_agentd.conf',
+      content => template('zabbix/zabbix_agentd.conf.erb'),
+      require => Package['zabbix-agent'],
+      notify  => Service['zabbix-agent']
+    }
 
-  service { 'zabbix-agent':
-    ensure     => running,
-    enable     => true,
-    require    => File['zabbix-agent-conf']
+    service { 'zabbix-agent':
+      ensure     => running,
+      enable     => true,
+      require    => File['zabbix-agent-conf']
+    }
   }
-}
--- manifests/server.pp
+++ manifests/server.pp
@@ -2,38 +2,38 @@
 define zabbix::server (
   $php_timezone = 'Japan/Tokyo',
   ){
+    include zabbix::repo
+    package { 'zabbix-release':
+      require => Apt::Source['zabbix']
+    }
+    package { 'zabbix-server-mysql':
+      require => Apt::Source['zabbix']
+    }
+    package { 'zabbix-frontend-php':
+      require => Apt::Source['zabbix']
+    }
 
-  package { 'zabbix-release':
-    require => Apt::Source['zabbix']
-  }
-  package { 'zabbix-server-mysql':
-    require => Apt::Source['zabbix']
-  }
-  package { 'zabbix-frontend-php':
-    require => Apt::Source['zabbix']
-  }
+    file { '/etc/apache2/conf.d/zabbix':
+      content => template('zabbix/apache_conf_zabbix.erb'),
+      require => Package['zabbix-frontend-php'],
+      notify  => Service['apache2']
+    }
 
-  file { '/etc/apache2/conf.d/zabbix':
-    content => template('zabbix/apache_conf_zabbix.erb'),
-    require => Package['zabbix-frontend-php'],
-    notify  => Service['apache2']
-  }
+    file { '/etc/zabbix/web/zabbix.conf.php':
+      content => template('zabbix/web_zabbix.conf.php.erb'),
+      require => Package['zabbix-frontend-php'],
+      notify  => Service['apache2']
+    }
 
-  file { '/etc/zabbix/web/zabbix.conf.php':
-    content => template('zabbix/web_zabbix.conf.php.erb'),
-    require => Package['zabbix-frontend-php'],
-    notify  => Service['apache2']
-  }
+    service { 'zabbix-server':
+      ensure  => running,
+      enable  => true,
+      require => Package['zabbix-server-mysql']
+    }
 
-  service { 'zabbix-server':
-    ensure  => running,
-    enable  => true,
-    require => Package['zabbix-server-mysql']
+    service { 'mysql':
+      ensure  => running,
+      enable  => true,
+      require => Package['zabbix-server-mysql']
+    }
   }
-
-  service { 'mysql':
-    ensure  => running,
-    enable  => true,
-    require => Package['zabbix-server-mysql']
-  }
-}
