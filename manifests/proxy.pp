41239f65b5fc93be7c8de4fcef9bb86df3590b55
hirocaster <hohtsuka@gmail.com>
2013-11-17 01:00:00 +0900

    Add flag official repository

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,8 +1,11 @@
 # zabbix-proxy class.
 define zabbix::proxy (
   $db = 'sqlite3',
+  $official_repository = true,
   ){
-    include zabbix::repo
+    if $official_repository == true {
+      include zabbix::repo
+    }
     package { 'zabbix-proxy-sqlite3':
       require => Apt::Source['zabbix']
     }
